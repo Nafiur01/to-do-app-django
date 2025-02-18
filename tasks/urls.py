7ca7f1bb099fd9058ad033e91315c6242758980e
Nafiur01 <nafiurrahman373@gmail.com>
2025-02-18 23:28:07 +0600

    update functionalities done

--- tasks/urls.py
+++ tasks/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.index,name='index')
+    path('',views.index,name='list'),
+    path('update-task/<str:pk>',views.update,name='update'),
+    path('delete-task/<str:pk>',views.delete,name='delete')
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,12 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from .models import *
+from .forms import *
 
 
 # Create your views here.
 
 def index(request):
+    form = TaskForm()
+
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('/')
     tasks = Tasks.objects.all()
-    context = {'tasks':tasks}
+    context = {'tasks':tasks,'form':form}
 
     return render(request,'tasks/list.html',context)
+
+
+def update(request,pk):
+    task = Tasks.objects.get(id=pk)
+    
+    form = TaskForm(instance=task)
+    context = {'form':form}
+
+    if request.method == 'POST':
+        form = TaskForm(request.POST,instance=task)
+        if form.is_valid():
+            form.save()
+            return redirect("/")
+
+    return render(request,'tasks/update.html',context)
+
+
+def delete(request,pk):
+    item = Tasks.objects.get(id=pk)
+
+    context = {'item':item}
+
+
+    return render(request,'tasks/delete.html',context)
